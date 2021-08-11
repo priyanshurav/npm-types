c4a0cba1c4e3158a932e4b46782b9b802311e6c5
Priyanshu Rav <77456300+priyanshurav@users.noreply.github.com>
2021-08-11 22:25:59 +0530

    Fixed a bug that prevented subsequent packages from being installed if one of them cannot be installed

--- src/utils.ts
+++ src/utils.ts
@@ -1,93 +1,86 @@
-import { spawn } from 'child_process';
-import fetch from 'node-fetch';
-import validate from 'validate-npm-package-name';
-import { info } from './logger';
-import { PackagesToBeInstalled } from './types';
+import { spawn } from "child_process";
+import fetch from "node-fetch";
+import validate from "validate-npm-package-name";
+import { info } from "./logger";
+import { PackagesToBeInstalled } from "./types";
 
 /* istanbul ignore next */
 export function runNpm(cmd: string, cwd = process.cwd()) {
-	return new Promise<Error | string>((resolve, reject) => {
-		const cmds = cmd.split(' ');
-		const proc = spawn(process.platform === 'win32' ? 'npm.cmd' : 'npm', cmds, {
-			cwd,
-		});
-		proc.on('exit', () => {
-			resolve(proc.stdout.read()?.toString?.('utf8'));
-		});
-		proc.on('error', (e) => {
-			reject(e);
-		});
-	});
+  return new Promise<Error | string>((resolve, reject) => {
+    const cmds = cmd.split(" ");
+    const proc = spawn(process.platform === "win32" ? "npm.cmd" : "npm", cmds, {
+      cwd,
+    });
+    proc.on("exit", () => {
+      resolve(proc.stdout.read()?.toString?.("utf8"));
+    });
+    proc.on("error", (e) => {
+      reject(e);
+    });
+  });
 }
 
-export async function queryPackageInfo(
-	pkg: string
-): Promise<Record<string, any>> {
-	const REQUEST_URL = `https://registry.npmjs.org/${pkg}`;
+export async function queryPackageInfo(pkg: string): Promise<Record<string, any>> {
+  const REQUEST_URL = `https://registry.npmjs.org/${pkg}`;
 
-	const res = await fetch(REQUEST_URL);
-	if (!res.ok) {
-		throw new Error(
-			`request to url '${REQUEST_URL}' failed with error code ${res.status}`
-		);
-	}
-	const json = await res.json();
-	return json;
+  const res = await fetch(REQUEST_URL);
+  if (!res.ok) {
+    throw new Error(`request to url '${REQUEST_URL}' failed with error code ${res.status}`);
+  }
+  const json = await res.json();
+  return json;
 }
 
 export function validatePackageName(pkg: string): boolean {
-	const res = validate(pkg);
+  const res = validate(pkg);
 
-	return res.validForNewPackages && res.validForOldPackages;
+  return res.validForNewPackages && res.validForOldPackages;
 }
 
 export async function getPkgsToBeInstalled(
-	pkgs: string[],
-	installDeprecatedTypes?: boolean
+  pkgs: string[],
+  installDeprecatedTypes?: boolean
 ): Promise<PackagesToBeInstalled> {
-	pkgs = [...new Set(pkgs)];
+  pkgs = [...new Set(pkgs)];
 
-	const deps: PackagesToBeInstalled = {
-		devDeps: [],
-		prodDeps: [],
-	};
+  const deps: PackagesToBeInstalled = {
+    devDeps: [],
+    prodDeps: [],
+  };
 
-	for (const pkg of pkgs) {
-		try {
-			if (!validatePackageName(pkg)) {
-				info(`'${pkg}' was skipped because the package name is invalid`);
-				break;
-			}
+  for (const pkg of pkgs) {
+    try {
+      if (!validatePackageName(pkg)) {
+        info(`'${pkg}' was skipped because the package name is invalid`);
+        continue;
+      }
 
-			const pkgInfo = await queryPackageInfo(pkg);
+      const pkgInfo = await queryPackageInfo(pkg);
 
-			deps.prodDeps.push(pkg);
+      deps.prodDeps.push(pkg);
 
-			const last = function <T>(arr: T[]): T {
-				return arr[arr.length - 1];
-			};
+      const last = function <T>(arr: T[]): T {
+        return arr[arr.length - 1];
+      };
 
-			if (pkgInfo.versions[last(Object.keys(pkgInfo.versions))]?.types) {
-				info(
-					`'${pkg}' has its built-in types, skipping its installation of types`
-				);
-				break;
-			}
+      if (pkgInfo.versions[last(Object.keys(pkgInfo.versions))]?.types) {
+        info(`'${pkg}' has its built-in types, skipping its installation of types`);
+        continue;
+      }
 
-			const pkgTypesInfo = await queryPackageInfo(`@types/${pkg}`);
+      const pkgTypesInfo = await queryPackageInfo(`@types/${pkg}`);
 
-			if (
-				pkgTypesInfo.versions[last(Object.keys(pkgTypesInfo.versions))]
-					?.deprecated &&
-				!installDeprecatedTypes
-			) {
-				info(`'@types/${pkg}' is deprecated, skipping its installation`);
-				break;
-			}
+      if (
+        pkgTypesInfo.versions[last(Object.keys(pkgTypesInfo.versions))]?.deprecated &&
+        !installDeprecatedTypes
+      ) {
+        info(`'@types/${pkg}' is deprecated, skipping its installation`);
+        continue;
+      }
 
-			deps.devDeps.push(`@types/${pkg}`);
-		} catch (e) {}
-	}
+      deps.devDeps.push(`@types/${pkg}`);
+    } catch (e) {}
+  }
 
-	return deps;
+  return deps;
 }
--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,52 +1,53 @@
-import { expect } from 'chai';
-import {
-	getPkgsToBeInstalled,
-	queryPackageInfo,
-	validatePackageName,
-} from '../src/utils';
+import { expect } from "chai";
+import { getPkgsToBeInstalled, queryPackageInfo, validatePackageName } from "../src/utils";
 
-describe('Test utils', () => {
-	describe('#getPkgsToBeInstalled()', () => {
-		it('should get correct packages to be installed', async () => {
-			const result = await getPkgsToBeInstalled(['mocha']);
-			const result2 = await getPkgsToBeInstalled(['get-text-indices']);
-			const result3 = await getPkgsToBeInstalled(['get-text-indic:es']);
-			const result4 = await getPkgsToBeInstalled(['chalk']);
-			const result5 = await getPkgsToBeInstalled(['jkahkjdf']);
+describe("Test utils", () => {
+  describe("#getPkgsToBeInstalled()", () => {
+    it("should get correct packages to be installed", async () => {
+      const result = await getPkgsToBeInstalled(["mocha"]);
+      const result2 = await getPkgsToBeInstalled(["get-text-indices"]);
+      const result3 = await getPkgsToBeInstalled(["get-text-indic:es"]);
+      const result4 = await getPkgsToBeInstalled(["chalk"]);
+      const result5 = await getPkgsToBeInstalled(["jkahkjdf"]);
+      const result6 = await getPkgsToBeInstalled(["chalk", "mocha"]);
 
-			expect(result).to.deep.equal({
-				devDeps: ['@types/mocha'],
-				prodDeps: ['mocha'],
-			});
-			expect(result2).to.deep.equal({
-				devDeps: [],
-				prodDeps: ['get-text-indices'],
-			});
-			expect(result3).to.deep.equal({
-				devDeps: [],
-				prodDeps: [],
-			});
-			expect(result4).to.deep.equal({
-				devDeps: [],
-				prodDeps: ['chalk'],
-			});
-			expect(result5).to.deep.equal({
-				devDeps: [],
-				prodDeps: [],
-			});
-		});
-	});
+      expect(result).to.deep.equal({
+        devDeps: ["@types/mocha"],
+        prodDeps: ["mocha"],
+      });
+      expect(result2).to.deep.equal({
+        devDeps: [],
+        prodDeps: ["get-text-indices"],
+      });
+      expect(result3).to.deep.equal({
+        devDeps: [],
+        prodDeps: [],
+      });
+      expect(result4).to.deep.equal({
+        devDeps: [],
+        prodDeps: ["chalk"],
+      });
+      expect(result5).to.deep.equal({
+        devDeps: [],
+        prodDeps: [],
+      });
+      expect(result6).to.deep.equal({
+        devDeps: ["@types/mocha"],
+        prodDeps: ["chalk", "mocha"],
+      });
+    });
+  });
 
-	describe('#queryPackageInfo()', async () => {
-		it('should query package info without errors', async () => {
-			const result = await queryPackageInfo('get-text-indices');
-			expect(typeof result).to.equal('object');
-		});
-	});
+  describe("#queryPackageInfo()", async () => {
+    it("should query package info without errors", async () => {
+      const result = await queryPackageInfo("get-text-indices");
+      expect(typeof result).to.equal("object");
+    });
+  });
 
-	describe('#validatePackageName()', () => {
-		const result = validatePackageName('jhdkhfk:%$khas');
+  describe("#validatePackageName()", () => {
+    const result = validatePackageName("jhdkhfk:%$khas");
 
-		expect(result).to.be.false;
-	});
+    expect(result).to.be.false;
+  });
 });
